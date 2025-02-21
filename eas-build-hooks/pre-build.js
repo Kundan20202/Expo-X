db99ba541bea40ec6d88084d934e5f887b2f4fad
Kundan20202 <86818363+Kundan20202@users.noreply.github.com>
2025-02-21 06:27:01 +0530

    Add files via upload

---
+++ eas-build-hooks/pre-build.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const gradleWrapperFilePath = path.join(
+  process.env.EAS_BUILD_WORKINGDIR || '.',
+  'android',
+  'gradle',
+  'wrapper',
+  'gradle-wrapper.properties'
+);
+
+const targetVersion = '8.2-all';
+
+if (fs.existsSync(gradleWrapperFilePath)) {
+  console.log('Updating Gradle version in gradle-wrapper.properties...');
+  const content = fs.readFileSync(gradleWrapperFilePath, 'utf8');
+  const updatedContent = content.replace(/gradle-[\d.]+-all.zip/, `gradle-${targetVersion}.zip`);
+  fs.writeFileSync(gradleWrapperFilePath, updatedContent, 'utf8');
+  console.log('Gradle version updated successfully!');
+} else {
+  console.log('gradle-wrapper.properties file not found. Skipping update.');
+}
